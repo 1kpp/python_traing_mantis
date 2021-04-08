80a9338745e1b6b708d1dc71502387a41f0974d1
1kpp <kpp11025@gmail.com>
2021-04-08 12:41:38 +0300

    Tests for adding and deleting projects were modified
    Lists of projects are being withdrown via SOAP

---
+++ fixture/soap.py
@@ -0,0 +1,31 @@
+from suds.client import Client
+from suds import WebFault
+from model.project import Project
+
+
+class SoapHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def can_login(self, username, password):
+        client = Client("http://localhost/mantisbt-2.25.0/api/soap/mantisconnect.php?wsdl#op.idp643324792")
+        try:
+            client.service.mc_login(username, password)
+            return True
+        except WebFault:
+            return False
+
+    def get_projects_list(self):
+        list = []
+        client = Client("http://localhost/mantisbt-2.25.0/api/soap/mantisconnect.php?wsdl#op.idp643324792")
+        try:
+            raw_data = client.service.mc_projects_get_user_accessible(self.app.config['web']['username'], self.app.config['web']['password'])
+            for row in raw_data:
+                id = row["id"]
+                name = row["name"]
+                description = row["description"]
+                list.append(Project(id=id, name=name, description=description))
+            return list
+        except WebFault:
+            return False
+
--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,10 +1,10 @@
 from model.project import Project
 
 
-def test_add_project(app, db, json_projects):
-    old_list_of_projects = db.get_projects_list()
+def test_add_project(app, json_projects):
+    old_list_of_projects = app.soap.get_projects_list()
     project_data = json_projects
     app.project.add_project(project_data)
-    new_list_of_projects = db.get_projects_list()
+    new_list_of_projects = app.soap.get_projects_list()
     old_list_of_projects.append(project_data)
     assert sorted(old_list_of_projects, key=Project.max_id) == sorted(new_list_of_projects, key=Project.max_id)
